ebd29bd7af03ac28da0b3d718e1b144eaa361e0d
joshelias <josh.elias.dev@gmail.com>
2014-06-27 15:01:14 -0400

    Generalized code
    Created a class structure for Entities with general routes, controllers
    and templates

--- js/app.js
+++ js/app.js
@@ -1,94 +1,59 @@
-window.Spark = Ember.Application.create();
+window.Spark = Ember.Application.create({
+  LOG_TRANSITIONS : true
+});
+
+// MODEL
 
 Spark.ApplicationAdapter = DS.FixtureAdapter.extend();
 
-// Router
 
-Spark.Router.map( function() {
-  /*
-  this.route("login");
-  this.resource("index", function() {
-    this.resource("settings", function() {
-
-    });
-  });
-  */
-	this.resource("games", { path: "/" }, function() {
-		this.resource("game", { path: ":game_id"}, function() {
-      this.route("edit");
-    });
-    this.route("new");
-	});
-  /*
-  this.resource("promoVideos", function() {
-    this.resource("promoVideo", { path: ":promoVideo_hid"});
-  })
-*/
+Spark.GameFilterOption = DS.Model.extend({
+  name : DS.attr('string'),
+  filter: DS.attr('string')
 });
 
+Spark.GameFilterOption.FIXTURES = [
+{
+  id : 1,
+  name: "All",
+  filter: "||||||||||||||||",
+},
+{
+  id : 2,
+  name: "Arcade",
+  filter: "||||||||||||||||",
+},
+{
+  id : 3,
+  name: "Recently Created",
+  filter: "||||||||||||||||",
+}
+];
 
-// GAMES
 
-
-Spark.GamesRoute = Ember.Route.extend({
-  model: function() {
-    var promises = {
-        model : this.store.find("game"),
-        filterOptions : this.store.find("filterOption"),
-        sortOptions : this.store.find("sortOption") 
-    }
-
-    return Ember.RSVP.hash(promises);  
-  },
-  setupController : function( controller, hash ) {
-      controller.setProperties({
-        model : hash.model,
-        filterOptions : hash.filterOptions,
-        sortOptions : hash.sortOptions
-      });
-  }
+Spark.GameSortOption = DS.Model.extend({
+  name : DS.attr('string'),
+  sort: DS.attr('string')
 });
 
+Spark.GameSortOption.FIXTURES = [
+{
+  id : 1,
+  name: "Created Date",
+  sort: "||||||||||||||||",
+},
 
-
-Spark.GamesController = Ember.ArrayController.extend({
-  filterOptions : {},
-  selectedFilters : [],
-  sortOptions : {},
-  selectedSorts : [],
-
-  selectedItems : [],
-
-  actions: {
-    // Game Actions
-    selectAllGames: function() {
-
-    },
-    newGame : function() {
-
-    },
-    deleteGame : function() {
-
-    },
-    refresh : function() {
-
-    },
-
-    // Sort/Filter Actions
-    showSortFilter : function() {
-
-    },
-    filterBy : function() {
-
-    },
-    sortBy : function() {
-
-    }
-  }
-})
-
-
-// GAME
+{
+  id : 2,
+  name: "Last Modified Date",
+  sort: "||||||||||||||||",
+},
+{
+  id : 3,
+  name: "Company",
+  sort: "||||||||||||||||",
+}
+];
 
 Spark.Game = DS.Model.extend({
   name : DS.attr('string'),
@@ -117,7 +82,85 @@ Spark.Game.FIXTURES = [
 }
 ];
 
-Spark.GameController = Ember.ObjectController.extend({
+
+
+// ENTITY ROUTE
+
+Spark.EntityListRoute = Ember.Route.extend({
+  model: function() {
+    var type = this.get("type");
+    var promises = {
+        model : this.store.find(type),
+        filterOptions : this.store.find(type+"FilterOption"),
+        sortOptions : this.store.find(type+"SortOption") 
+    }
+    return Ember.RSVP.hash(promises);  
+  },
+  setupController : function( controller, hash ) {
+    var type = this.get("type");
+    controller.setProperties({
+      model : hash.model,
+      filterOptions : hash.filterOptions,
+      sortOptions : hash.sortOptions,
+      type : type 
+    });
+  },
+  renderTemplate: function() {
+    var type = this.get("type");
+    this.render('entityList', { controller: type+'List' });
+  }
+});
+
+Spark.EntityRoute = Ember.Route.extend({
+  renderTemplate: function() {
+    var type = this.get("type");
+    this.render('entity', { controller: type });
+  }
+});
+
+
+
+// ENTITY CONTROLLERS
+
+Spark.EntityListController = Ember.ArrayController.extend({
+  type: undefined,
+
+  filterOptions : undefined,
+  selectedFilters : Ember.A(),
+  
+  sortOptions : undefined,
+  selectedSorts : Ember.A(),
+
+  selectedItems : Ember.A(),
+
+  actions: {
+    // Game Actions
+    selectAll: function() {
+
+    },
+    new : function() {
+
+    },
+    delete : function() {
+
+    },
+    refresh : function() {
+
+    },
+    // Sort/Filter Actions
+    showSortFilter : function() {
+
+    },
+    filterBy : function() {
+
+    },
+    sortBy : function() {
+
+    }
+  }
+})
+
+Spark.EntityController = Ember.ObjectController.extend({
   actions: {
     edit : function() {
 
@@ -132,131 +175,85 @@ Spark.GameController = Ember.ObjectController.extend({
 
     }
   }
+});
+
+
+// SELECT OPTION COMPONENTS
+
+Spark.SelectOptionListComponent = Ember.Component.extend({
+  options : undefined,
+  selected: undefined,
+  hasSelectAll: false,
+  actions : {
+    toggleSelected : function(optionID, isSelected) {
+      var selected = this.get("selected");
+      if(isSelected) {
+        selected.pushObject(optionID); 
+      } else {
+        selected.removeAt(selected.indexOf(optionID));
+      }
+
+      if( selected.length <= 0 ) {
+        this.set("hasSelectAll", false);
+      }
+    }
+  }
+});
+
+Spark.SelectOptionComponent = Ember.Component.extend({
+  isSelected: false, 
+  optionID: undefined, 
+  toggleSelected : function() {
+      var isSelected = this.get("isSelected");
+      var optionID = this.get("optionID");
+      this.sendAction("PA_toggleSelected", optionID, isSelected);
+  }.observes("isSelected"),
+  isSelectAllToggled : function() {
+    this.set( "isSelected", this.get("parentView.hasSelectAll") );
+  }.observes("parentView.hasSelectAll")
+});
+
+
+
+// Router
+
+Spark.Router.map( function() {
+  /*
+  this.route("login");
+  this.resource("index", function() {
+    this.resource("settings", function() {
+
+    });
+  });
+  */
+  this.resource("gameList", { path: "/" }, function() {
+    this.resource("game", { path: ":game_id"}, function() {
+      this.route("edit");
+    });
+    this.route("new");
+  });
+  /*
+  this.resource("promoVideos", function() {
+    this.resource("promoVideo", { path: ":promoVideo_hid"});
+  })
+*/
+});
+
+// GAME
+
+Spark.GameListRoute = Spark.EntityListRoute.extend({
+  type: "game"
+});
+Spark.GameRoute = Spark.EntityRoute.extend({
+  type: "game"
 })
 
-
-// FILTER
-
-Spark.FilterOption = DS.Model.extend({
-  name : DS.attr('string'),
-  filter: DS.attr('string')
-});
-
-Spark.FilterOption.FIXTURES = [
-{
-  id : 1,
-  name: "All",
-  filter: "||||||||||||||||",
-},
-{
-  id : 2,
-  name: "Arcade",
-  filter: "||||||||||||||||",
-},
-{
-  id : 3,
-  name: "Recently Created",
-  filter: "||||||||||||||||",
-}
-];
+Spark.GameListController = Spark.EntityListController.extend();
+Spark.GameController = Spark.EntityController.extend();
 
 
-Spark.FilterOptionsComponent = Ember.Component.extend({
-  filterOptions : undefined,
-  selected: undefined,
-  hasSelectAll: false,
-  actions : {
-    toggleSelected : function(optionID, isSelected) {
-      var selected = this.get("selected");
-      if(isSelected) {
-        selected.pushObject(optionID); 
-      } else {
-        selected.removeAt(selected.indexOf(optionID));
-      }
+Spark.FilterOptionListComponent = Spark.SelectOptionListComponent.extend();
+Spark.FilterOptionComponent = Spark.SelectOptionComponent.extend();
 
-      if( selected.length <= 0 ) {
-        this.set("hasSelectAll", false);
-      }
-    },
-    refresh : function() {
-
-    } 
-  }
-});
-
-Spark.FilterOptionComponent = Ember.Component.extend({
-  isSelected: false, //Ember.computed.oneWay('parentView.hasSelectAll'),
-  optionID: undefined, 
-  toggleSelected : function() {
-      var isSelected = this.get("isSelected");
-      var optionID = this.get("optionID");
-      this.sendAction("PA_toggleSelected", optionID, isSelected);
-  }.observes("isSelected"),
-  isSelectAllToggled : function() {
-    this.set( "isSelected", this.get("parentView.hasSelectAll") );
-  }.observes("parentView.hasSelectAll")
-});
-
-
-// SORT
-
-Spark.SortOption = DS.Model.extend({
-  name : DS.attr('string'),
-  sort: DS.attr('string')
-});
-
-Spark.SortOption.FIXTURES = [
-{
-  id : 1,
-  name: "Created Date",
-  sort: "||||||||||||||||",
-},
-
-{
-  id : 2,
-  name: "Last Modified Date",
-  sort: "||||||||||||||||",
-},
-{
-  id : 3,
-  name: "Company",
-  sort: "||||||||||||||||",
-}
-];
-
-
-Spark.SortOptionsComponent = Ember.Component.extend({
-  sortOptions : undefined,
-  selected: undefined,
-  hasSelectAll: false,
-  actions : {
-    toggleSelected : function(optionID, isSelected) {
-      var selected = this.get("selected");
-      if(isSelected) {
-        selected.pushObject(optionID); 
-      } else {
-        selected.removeAt(selected.indexOf(optionID));
-      }
-
-      if( selected.length <= 0 ) {
-        this.set("hasSelectAll", false);
-      }
-    },
-    refresh : function() {
-
-    } 
-  }
-});
-
-Spark.SortOptionComponent = Ember.Component.extend({
-  isSelected: false, //Ember.computed.oneWay('parentView.hasSelectAll'),
-  optionID: undefined, 
-  toggleSelected : function() {
-      var isSelected = this.get("isSelected");
-      var optionID = this.get("optionID");
-      this.sendAction("PA_toggleSelected", optionID, isSelected);
-  }.observes("isSelected"),
-  isSelectAllToggled : function() {
-    this.set( "isSelected", this.get("parentView.hasSelectAll") );
-  }.observes("parentView.hasSelectAll")
-});
+Spark.SortOptionListComponent = Spark.SelectOptionListComponent.extend();
+Spark.SortOptionComponent = Spark.SelectOptionComponent.extend();
